e285ce0d663201f51940d93faf8197d53529b5e7
Bobzero23 <rajabu.mustafa.00@gmail.com>
2024-08-01 12:54:15 +0300

    quick fix: unneccesay feature removed

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,7 @@ import ProductList from "@/components/ProductList";
 
 const page = () => {
   return (
-    <div className="font-extrabold w-full text-center border border-pink-500 my-1 p-1">
+    <div className="w-full text-center border border-pink-500 my-1 p-1">
       <ProductList />
     </div>
   );
