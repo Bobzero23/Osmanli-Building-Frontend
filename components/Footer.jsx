037183df7b2343adafd5c2f7443e51b97573e2ae
Bobzero23 <rajabu.mustafa.00@gmail.com>
2024-08-01 09:44:13 +0300

    setting up Footer component ready for future desiging and implementation

---
+++ components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="flex-center border border-blue-600 w-full h-10">
+      İLETİŞİM BİLGİLER
+    </div>
+  );
+};
+
+export default Footer;
