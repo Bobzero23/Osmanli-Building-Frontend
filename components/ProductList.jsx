26bec35b51f62d66aa8b0261279294fcf0e6a452
Bobzero23 <rajabu.mustafa.00@gmail.com>
2024-08-01 10:45:04 +0300

    displaying the products in the productList component

--- components/ProductList.jsx
+++ components/ProductList.jsx
@@ -1,7 +1,16 @@
-import React from "react";
+import ProductCard from "./ProductCard";
 
 const ProductList = () => {
-  return <div>ProductList</div>;
+  return (
+    <div className=" w-full flex gap-6 flex-wrap flex-center overflow-y-auto h-[calc(100vh-100px)]">
+      {[
+        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+      ].map((card) => (
+        <ProductCard />
+      ))}
+    </div>
+  );
 };
 
 export default ProductList;
