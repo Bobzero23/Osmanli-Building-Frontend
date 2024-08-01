5e995533e70db94d248a0189443d564003dba257
Bobzero23 <rajabu.mustafa.00@gmail.com>
2024-08-01 13:50:23 +0300

    feature: displaying demo collections

--- app/collection/page.jsx
+++ app/collection/page.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 
 const CellectionCard = () => {
-  return <div></div>;
+  return (
+    <div className="border border-b-slate-700">
+      <div className="w-[300px] h-[250px]"> Collection Card</div>
+    </div>
+  );
 };
 
 const Collection = () => {
   return (
-    <div>
-      {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((card) => (
+    <div className="flex-center gap-1 flex-wrap p-2 border border-b-slate-700">
+      {[1, 1, 1, 1, 1, 1].map((card) => (
         <CellectionCard />
       ))}
     </div>
