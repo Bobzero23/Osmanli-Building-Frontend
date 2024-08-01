320830df84b77085b7b15c821bd3a49605d7eea9
Bobzero23 <rajabu.mustafa.00@gmail.com>
2024-08-01 10:44:25 +0300

    designing the ProductCard component ready for styling

---
+++ components/ProductCard.jsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+
+const ProductCard = () => {
+  return (
+    <div className="border border-green-800 p-1">
+      <div className="flex flex-col">
+        <div className="flex flex-col">
+          <Image
+            className="object-cover"
+            alt="Product image"
+            width={300}
+            height={300}
+            src="/assets/images/test_image.jpg"
+          />
+          <div>
+            <p className="font-thin pt-1">Product name</p>
+          </div>
+        </div>
+        <div className="flex items-center justify-between p-2 border border-green-500">
+          <div>12,000</div>
+          <button className="black_btn">Add to cart</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
