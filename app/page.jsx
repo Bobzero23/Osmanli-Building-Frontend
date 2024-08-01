47c961d38ef68296bdb7103d10a32d559872c54f
Bobzero23 <rajabu.mustafa.00@gmail.com>
2024-08-01 09:43:33 +0300

    setting up the home page ready and visible for better designing

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const page = () => {
-  return <div className="font-extrabold w-full text-center">page</div>;
+  return (
+    <div className="font-extrabold w-full text-center border border-pink-500 my-1">
+      page
+    </div>
+  );
 };
 
 export default page;
