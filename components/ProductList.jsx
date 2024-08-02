8125305fec53d13cdfff689357211b5550035f1a
Bobzero23 <rajabu.mustafa.00@gmail.com>
2024-08-02 12:13:50 +0300

    feature: decreasing the products and adding the key property

--- components/ProductList.jsx
+++ components/ProductList.jsx
@@ -2,12 +2,11 @@ import ProductCard from "./ProductCard";
 
 const ProductList = () => {
   return (
-    <div className="flex gap-5 flex-wrap justify-center w-auto p-2 shrink-1">
+    <div className="flex gap-3 flex-wrap justify-center w-auto p-2 shrink-1">
       {[
         1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-      ].map((card) => (
-        <ProductCard />
+      ].map((card, i) => (
+        <ProductCard key={i} />
       ))}
     </div>
   );
