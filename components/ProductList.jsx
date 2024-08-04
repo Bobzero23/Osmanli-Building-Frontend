e432feeb3498c170eb94a69a3a72aa4a433388f3
Bobzero23 <rajabu.mustafa.00@gmail.com>
2024-08-04 17:15:13 +0300

    styling : using grid for product displaying

--- components/ProductList.jsx
+++ components/ProductList.jsx
@@ -2,7 +2,7 @@ import ProductCard from "./ProductCard";
 
 const ProductList = () => {
   return (
-    <div className="flex gap-3 flex-wrap justify-center w-auto p-2 shrink-1">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 xl:grid-cols-4">
       {[
         1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
       ].map((card, i) => (
