7b2f57cc19b5ab8d7b2eec795a4f04a26e125d81
Bobzero23 <rajabu.mustafa.00@gmail.com>
2024-08-06 09:59:52 +0300

    user login api using zustand (global state management)

---
+++ store/store.js
@@ -0,0 +1,27 @@
+import { api, BASE_URL } from "@/api/api";
+import axios from "axios";
+import { create } from "zustand";
+
+const authStore = create((set) => ({
+  user: null,
+  userId: null,
+  loggedIn: false,
+  loading: false,
+  jwt: null,
+  users: [],
+
+  login: async (userData) => {
+    set({ loading: true });
+
+    try {
+      const { data } = await axios.post(`${BASE_URL}auth/signin`, userData);
+      localStorage.setItem("jwt", data.jwt);
+      set({ loading: false, loggedIn: true, jwt: data.jwt });
+      return data;
+    } catch (error) {
+      set({ error: error, loading: false });
+    }
+  },
+}));
+
+export default authStore;
