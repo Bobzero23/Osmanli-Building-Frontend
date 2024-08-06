f5f23ce03ad521e9e4e030e66e16d5e00fcebde1
Bobzero23 <rajabu.mustafa.00@gmail.com>
2024-08-06 11:48:34 +0300

    feature: adding logout button and logout functionality with notification

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,8 +1,24 @@
+"use client";
 import Link from "next/link";
 import Image from "next/image";
 import React from "react";
+import authStore from "@/store/AuthStore";
+import toast, { Toaster } from "react-hot-toast";
+import { useRouter } from "next/navigation";
 
 const Header = () => {
+  const route = useRouter();
+  const logout = authStore((state) => state.logout);
+  const isLoggedIn = authStore((state) => state.loggedIn);
+  console.log(isLoggedIn);
+
+  const hanldeLogout = () => {
+    logout();
+    toast.success("Signed out successfully!!");
+    setTimeout(() => {
+      route.push("/");
+    }, 1000);
+  };
   return (
     <div className="glassmorphism mb-2 flex flex-col justify-center items-center w-full p-3 gap-2">
       <div className="w-full hidden md:flex flex-center gap-3 p-1  glassmorphism">
@@ -72,12 +88,23 @@ const Header = () => {
               src="/assets/icons/cart.png"
             />
           </Link>
-          <Link href="/sign-in" className="orange_btn_rounded flex-shrink">
-            Login
-          </Link>
+          {isLoggedIn ? (
+            <Link
+              href="/"
+              className="orange_btn_rounded flex-shrink"
+              onClick={hanldeLogout}
+            >
+              Logout
+            </Link>
+          ) : (
+            <Link href="/sign-in" className="orange_btn_rounded flex-shrink">
+              Login
+            </Link>
+          )}
         </div>
       </div>
       {/* TODO - Mobile Navigation */}
+      <Toaster />
     </div>
   );
 };
