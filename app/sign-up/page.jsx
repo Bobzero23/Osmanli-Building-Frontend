cd3480fa9ee2225f4ceb47408106f20208508ba4
Bobzero23 <rajabu.mustafa.00@gmail.com>
2024-08-06 10:20:38 +0300

    implementing signup functionality inside singup component

--- app/sign-up/page.jsx
+++ app/sign-up/page.jsx
@@ -1,19 +1,54 @@
+"use client";
+import authStore from "@/store/store";
 import { Button, TextField } from "@mui/material";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 const SignUp = () => {
+  const route = useRouter();
+  const signup = authStore((state) => state.signup);
+
+  const [formData, setformData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setformData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const result = await signup(formData);
+    if (result) console.log(result);
+    route.push("/");
+  };
+
   return (
     <>
       <div className="flex items-center justify-center h-screen px-3">
         <div>
           <h1 className="text-lg font-bold text-center pb-8">SIGN UP</h1>
           <form className="space-y-3">
-            <TextField fullWidth required name="username" label="username" />
+            <TextField
+              fullWidth
+              required
+              name="username"
+              label="username"
+              onChange={handleChange}
+            />
             <TextField
               fullWidth
               required
               name="email"
               label="email"
               type="email"
+              onChange={handleChange}
             />
             <TextField
               fullWidth
@@ -21,9 +56,15 @@ const SignUp = () => {
               name="password"
               label="password"
               type="password"
+              onChange={handleChange}
             />
             <div>
-              <Button fullWidth type="submit" variant="contained">
+              <Button
+                fullWidth
+                type="submit"
+                variant="contained"
+                onClick={handleSubmit}
+              >
                 REGISTER
               </Button>
             </div>
