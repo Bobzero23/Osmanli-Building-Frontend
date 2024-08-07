129f84b6d257d500c701874715be1432db948ee1
Bobzero23 <rajabu.mustafa.00@gmail.com>
2024-08-07 12:50:33 +0300

    feature: refactoring the footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -6,11 +6,39 @@ import XIcon from "@mui/icons-material/X";
 
 const Footer = () => {
   return (
-    <div className="flex-center glassmorphism flex-col mt-4 gap-2 w-full h-auto bg-gradient-to-r from-orange-400 via-red-200 to-orange-400">
-      <div className="gap-2 glassmorphism">
+    <div className="flex-center flex-col pb-5 mt-4 gap-2 w-full h-auto bg-gradient-to-r from-orange-400 via-red-200 to-orange-400">
+      <div className="gap-2">
         <h1 className="head_text orange_gradient w-full flex-center">
           GET IN TOUCH WITH US
         </h1>
+
+        <ul className=" md:flex md:price_glassmorphism justify-center w-full">
+          <li className="list-none">
+            <Link href="/">
+              <p className="outline_btn ">Home</p>
+            </Link>
+          </li>
+          <li className="">
+            <Link href="/collection">
+              <p className="outline_btn ">Collection</p>
+            </Link>
+          </li>
+          {/* <li className="">
+          <Link href="/our-services">
+            <p className="outline_btn ">Our Services</p>
+          </Link>
+        </li> */}
+          <li className="">
+            <Link href="/about-us">
+              <p className="outline_btn ">About Us</p>
+            </Link>
+          </li>
+          <li className="">
+            <Link href="/contact-us">
+              <p className="outline_btn ">Contact Us</p>
+            </Link>
+          </li>
+        </ul>
         <div className="flex-center w-full gap-3 ">
           <Link href="/">
             <WhatsAppIcon />
@@ -22,39 +50,7 @@ const Footer = () => {
             <XIcon />
           </Link>
         </div>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis
-          similique tempora sapiente aliquid eveniet. Iusto cumque.
-        </p>
       </div>
-
-      <ul className=" md:flex price_glassmorphism">
-        <li className="list-none">
-          <Link href="/">
-            <p className="outline_btn ">Home</p>
-          </Link>
-        </li>
-        <li className="">
-          <Link href="/collection">
-            <p className="outline_btn ">Collection</p>
-          </Link>
-        </li>
-        {/* <li className="">
-          <Link href="/our-services">
-            <p className="outline_btn ">Our Services</p>
-          </Link>
-        </li> */}
-        <li className="">
-          <Link href="/about-us">
-            <p className="outline_btn ">About Us</p>
-          </Link>
-        </li>
-        <li className="">
-          <Link href="/contact-us">
-            <p className="outline_btn ">Contact Us</p>
-          </Link>
-        </li>
-      </ul>
     </div>
   );
 };
