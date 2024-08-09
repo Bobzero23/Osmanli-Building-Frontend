792eb94f5c3f6a47ca5ae7189a4ccaa5f12cd412
Bobzero23 <rajabu.mustafa.00@gmail.com>
2024-08-09 11:29:11 +0300

    optimazation: instead of manually writing every sideBar element I used an array of objects and mapped all the sideItems I want to show

--- app/page.jsx
+++ app/page.jsx
@@ -1,35 +1,29 @@
 import ProductList from "@/components/ProductList";
 import Link from "next/link";
 
+const sideBar = [
+  { name: "PVC", category: "" },
+  { name: "ACCESSORIES", category: "pvc" },
+  { name: "HARDWARES", category: "hardware" },
+  { name: "GLASSWARES", category: "glassware" },
+  { name: "ALL PRODUCTS", category: "" },
+];
+
 const page = () => {
   return (
     <div className="p-0 flex gap-2 borderp-2 mb-3">
       <div className="p-1 md:flex hidden flex-col gap-2 w-[25%]">
-        <Link className="glassmorphism orange_hover w-full" href="/">
-          <p className="font-extrabold orange_gradient w-full h-full">PVC</p>
-        </Link>
-        <Link className="glassmorphism orange_hover w-full" href="/">
-          <p className="font-extrabold orange_gradient w-full h-full">
-            ACCESSORIES
-          </p>
-        </Link>
-        <Link className="glassmorphism orange_hover w-full" href="/">
-          <p className="font-extrabold orange_gradient w-full h-full">
-            HARDWARES
-          </p>
-        </Link>
-        <Link className="glassmorphism orange_hover w-full " href="/">
-          <p className="font-extrabold orange_gradient w-full h-full">
-            GLASSWARE
-          </p>
-        </Link>
-        <Link className="glassmorphism orange_hover w-ful" href="/">
-          <p className="font-extrabold orange_gradient w-full h-full">
-            SOMETHING
-          </p>
-        </Link>
+        {sideBar.map((sideItem, i) => (
+          <Link key={i} className="glassmorphism orange_hover w-full" href="/">
+            <p className="font-extrabold orange_gradient w-full h-full">
+              {sideItem.name}
+            </p>
+          </Link>
+        ))}
       </div>
+
       <div className="text-center flex-center mx-auto glassmorphism my-1 p-1 md:w-[75%] sm:w-full">
+        {/* TODO - add a prop here to display categor in the product list page */}
         <ProductList />
       </div>
     </div>
