cad19b2418aff154d8faf11b55e619483fe16979
Bobzero23 <rajabu.mustafa.00@gmail.com>
2024-08-13 10:10:24 +0300

    configuration: making nextJs-image component allowing image path from amazon s3 bucket

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "osmanlibuildingbobzero.s3.us-east-1.amazonaws.com",
+        port: "",
+        pathname: "/**", // This allows all paths under the hostname
+      },
+    ],
+  },
+};
 
 export default nextConfig;
