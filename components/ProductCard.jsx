10a43e9fae67c6044878e6e5681b4a33f17ed9ef
Bobzero23 <rajabu.mustafa.00@gmail.com>
2024-08-13 10:08:22 +0300

    feature: implementing dynamic properties which are being fetched from database and displaying them in productcard component

--- components/ProductCard.jsx
+++ components/ProductCard.jsx
@@ -1,24 +1,24 @@
 import Image from "next/image";
 
-const ProductCard = () => {
+const ProductCard = ({ product }) => {
   return (
     <div className="card_box card_hover p-1 sm:w-full md:w-auto sm:shrink md:flex">
       <div className="flex flex-col">
-        <div className="flex flex-col">
+        <div className="flex flex-col overflow-hidden">
           <Image
-            className="object-cover"
+            className="object-fit w-full h-full"
             alt="Product image"
             width={300}
             height={300}
-            src="/assets/images/test_image.jpg"
+            src={product.imageUrl || "/assets/images/test_image.jpg"}
           />
           <div>
-            <p className="font-thin pt-1">Product name</p>
+            <p className="font-thin pt-1">{product.name}</p>
           </div>
         </div>
         <div className="card_box flex items-center justify-between p-2">
           <div className="shrink price_glassmorphism text-green-600 font-bold">
-            $ 12,000
+            {product.price}
           </div>
           <button className="orange_btn">Add to cart</button>
         </div>
