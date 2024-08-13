2779d4a66acd05cc08dbdbb82ff5b5e1c2aca8d4
Bobzero23 <rajabu.mustafa.00@gmail.com>
2024-08-13 10:06:24 +0300

    feature: removing a demo list and fetching products from database and rendering them

--- components/ProductList.jsx
+++ components/ProductList.jsx
@@ -1,12 +1,23 @@
+"use client";
+
 import ProductCard from "./ProductCard";
+import { useEffect } from "react";
+import { useStore } from "zustand";
+import productStore from "@/store/ProductStore";
 
 const ProductList = () => {
+  const { products, getAllProducts, loading, error } = useStore(productStore);
+
+  useEffect(() => {
+    getAllProducts();
+    if (products) console.log(products);
+  }, []);
+
+  useEffect(() => {});
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 xl:grid-cols-4">
-      {[
-        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-      ].map((card, i) => (
-        <ProductCard key={i} />
+      {products?.map((product, i) => (
+        <ProductCard key={i} product={product} />
       ))}
     </div>
   );
